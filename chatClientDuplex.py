08ef8ff4b6d04b82882282712aed9ab0dcfc7b80
Rahul <rahul@DESKTOP-2MF8IF4.localdomain>
2020-12-02 16:20:20 -0800

    Diffie hellman, wip

--- chatClientDuplex.py
+++ chatClientDuplex.py
@@ -3,14 +3,69 @@ import sys
 import threading
 
 
-
 # If a quit message is received 'q' terminate the threads and close the program
 closeConnection = False
+
+# Diffie-hellman variables
+sharedPrime = 0
+sharedBase = 0
+serverValue = 0
+secret = 0
+sharedSecret = 0
+
+
+
+"""Perform diffie hellman key exchange"""
+def diffie_hellman(snd_conn, rcv_conn): 
+    global sharedPrime, sharedBase, serverValue, secret, sharedSecret
+    
+    print('\n\n--------------------Performing Diffie Hellman--------------------')
+    
+    # Client should receive 3 numbers from server
+    #   - sharedPrime 
+    #   - sharedBase
+    #   - a server calculated number based on the shared and secret keys
+    recvVals = 0
+    
+    # Get the prime modulus and generator from server 
+    while recvVals < 3:
+        
+        # Receives the message from the server
+        message = rcv_conn.recv(1024).decode()
+        
+        # If the message is not empty and not 'q', print it
+        if message != False:
+            if recvVals == 0:
+                print('Shared Prime received: ' + message)
+                sharedPrime = int(message)
+            elif recvVals == 1:
+                print('Shared base received: ' + message)
+                sharedBase = int(message)
+            elif recvVals == 2:
+                print('Server calculated value received: ' + message)
+                serverValue = int(message)
+            recvVals+=1
+         
+    # Calculate the value based on the shared and secret keys, and send it to client
+    secret = 15
+    clientValue = str((sharedBase ** secret) % sharedPrime)
+    print('Client sending calculated value: ' + clientValue)
+    snd_conn.sendall(clientValue.encode())
+    
+    # Calculate shared secret
+    sharedSecret = (serverValue**secret) % sharedPrime
+    print('Client Shared Secret calculated: ' + str(sharedSecret))
+
+        
+    
+
+
+
 
 
 
 """Sending message to server"""
-def send_to_server(clsock):
+def send_to_server(snd_conn):
     global closeConnection # both threads use this global variable
 
     # Loop forever until a 'q' message is sent or received
@@ -18,7 +73,7 @@ def send_to_server(clsock):
         
         # Get message from client and send to server
         send_msg = input('\nType Message: ')
-        clsock.sendall(send_msg.encode())
+        snd_conn.sendall(send_msg.encode())
         
         # If a quit message is sent, update flag and exit thread
         if send_msg == 'q':
@@ -28,27 +83,26 @@ def send_to_server(clsock):
            
 
 """Receiving message from server""" 
-def recv_from_server(conn):
+def recv_from_server(rcv_conn):
     global closeConnection # both threads use this global variable
     
     # Loop forever until a 'q' message is sent or received
     while closeConnection == False:
         
         # Receives the message from the server
-        message = conn.recv(1024).decode()
-        
-        # If quit message is received from server, update flag and exit thread
-        if message == 'q':
-            print('\nConnection closed by server')
-            conn.close()
-            closeConnection = True
+        message = rcv_conn.recv(1024).decode()
         
         # If the message is not empty and not 'q', print it
         if message != False:
            print('\nMessage Received: ' + message)
+        
+        # If quit message is received from server, update flag and exit thread
+        if message == 'q':
+            print('\nConnection closed by server')
+            closeConnection = True
+        
 
-           
-
+   
 """Main function"""
 def main():
     global closeConnection
@@ -61,31 +115,49 @@ def main():
     # Create a TCP client socket and a thread for sending messages
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((HOST, serverPort))
-    sndThread = threading.Thread(target = send_to_server, args = (clientSocket,))
     
     # Create a TCP socket, and a thread for receiving messages
     receivingSocket = socket(AF_INET, SOCK_STREAM)
+    receivingSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # can reuse address immediately after shutting down
     receivingSocket.bind(('', clientPort))
     receivingSocket.listen(1)
     clientconnection, addr = receivingSocket.accept()
+    print('-----------------------------------------------')
     print('Client connection with server established')
     print('Send \"q\" to close connection and quit program')
+    print('-----------------------------------------------')
     rcvThread = threading.Thread(target = recv_from_server, args = (clientconnection,))
+    
+    
+    
+    
+    # Perform diffie-hellman key exchange
+    diffie_hellman(clientSocket, clientconnection)
+    
+    
+    
+    
+    
+    
+    # Create two threads, one for receiving messages and one for sending messages
+    #   - two seperate threads so that we can send and receive messages at the same time
+    #rcvThread = threading.Thread(target = recv_from_server, args = (clientconnection,))
+    #sndThread = threading.Thread(target = send_to_server, args = (clientSocket,))
+
+    
     
     # Start the sending and receiving threads
     # Setting a daemons so that threads are killed when main program exits
-    sndThread.daemon = True
-    rcvThread.daemon = True
-    sndThread.start()
-    rcvThread.start()
+    #sndThread.daemon = True
+    #rcvThread.daemon = True
+    #sndThread.start()
+    #rcvThread.start()
     
     # Close the program when a quit message is sent or recieved
-    while(closeConnection == False):
-        if(closeConnection == True):
-            print('Program finished')
-            sys.exit()
-
-
+    #while(closeConnection == False):
+    #    if(closeConnection == True):
+    #        print('Program finished')
+    #        sys.exit()
 
 
 """Execute main function"""
--- chatServerDuplex.py
+++ chatServerDuplex.py
@@ -1,25 +1,78 @@
 from socket import *
 import sys
 import threading
-
-
+import time
+import sympy # Library for generating prime numbers
 
 # If a quit message is received 'q' terminate the threads and close the program
 closeConnection = False
 
 
 
+"""Generate random prime number to use in diffie hellman"""
+def generate_random_prime():
+    lowerBound = 100
+    upperBound = 999
+    primes = [i for i in range(lowerBound,upperBound) if isPrime(i)]
+    n = random.choice(primes)
+    return n
+
+
+
+"""Perform diffie hellman key exchange"""
+def diffie_hellman(snd_conn, rcv_conn):
+
+    print('\n\n--------------------Performing Diffie Hellman--------------------')
+    # Generate the values
+    prime = 23
+    base = 5
+    secret = 6
+    sendToClient = (base**secret) % prime
+    sharedPrime = str(23)
+    sharedBase = str(5)
+    sharedValue = str(sendToClient)
+    valueFromClient = 0
+    serverSharedSecret = 0
+    
+    # Send the values
+    print('Sending randomly generated shared prime to client: ' + sharedPrime)
+    print('Sending randomly generated shared base to client: '  + sharedBase)
+    print('Sending calculated value to client: ' + sharedValue)
+    snd_conn.send(sharedPrime.encode())
+    time.sleep(1)
+    snd_conn.send(sharedBase.encode())
+    time.sleep(1)
+    snd_conn.send(sharedValue.encode())
+    
+    # Get the calculated value from the client
+    while True:
+        valueFromClient = int(rcv_conn.recv(1024).decode())
+        break
+    print('Calculated value received from client: ' + str(valueFromClient))
+    
+    # Calculate the shared secret
+    serverSharedSecret = (valueFromClient ** secret) % int(sharedPrime)
+    print('\nServer Shared Secret calculated: ' + str(serverSharedSecret))
+    print('------------------------------------------------------------------')
+
+
+
+   
+    
+
+
+
 """Receiving message from client"""
-def recv_from_client(clsock):
+def recv_from_client(rcv_conn):
     global closeConnection # both threads use this global variable
 
     # Loop forver, terminate if a quit ("q") message is received
     while closeConnection == False:
-        data = clsock.recv(1024).decode()
+        data = rcv_conn.recv(1024).decode()
         
         # If server receives a "q", close the connection
         if data == 'q':
-            print('\Connection closed by client')
+            print('\nConnection closed by client')
             closeConnection = True
         
         # If the data is not a "q" and is not empty, print the message
@@ -29,28 +82,22 @@ def recv_from_client(clsock):
         
 
 """Sending message to client"""
-def send_to_client(conn):
+def send_to_client(snd_conn):
     global closeConnection # both threads use this global variable
 
     # Loop forver, terminate if a quit ("q") message is sent
     while closeConnection == False:
-        try:
-            # Input a message
-            send_msg = input('\nType Message: ')
+        # Input a message
+        send_msg = input('\nType Message: ')
             
-            # If the message is "q", close the connection after sending the message
-            if send_msg == 'q':
-                conn.send(send_msg.encode())
-                print('\nServer is closing connection')
-                conn.close()
-                closeConnection = True
-            else:
-                conn.send(send_msg.encode())
+        # If the message is "q", close the connection after sending the message
+        if send_msg == 'q':
+            snd_conn.send(send_msg.encode())
+            print('\nServer is closing connection')
+            closeConnection = True
+        else:
+            snd_conn.send(send_msg.encode())
         
-        # If an error occurs, close the connection
-        except:
-            conn.close()
-    
 
 
 """Main Function"""
@@ -68,38 +115,50 @@ def main():
     listeningSocket = socket(AF_INET, SOCK_STREAM)
     
     # Bind the listening socket to the port
+    listeningSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # can reuse address immediately after shutting down
     listeningSocket.bind(('', listeningPort))
     
     # Listen for at most 1 connection
     listeningSocket.listen(1) 
+    print('--------------------------------------------')
     print('Server is listening for incoming connections')
     
     # Accept a connection request from the client
     connectionSocket, addr = listeningSocket.accept()
     print('Server has connected with a client')
     print('Send \"q\" to close connection and quit program')
+    print('--------------------------------------------')
 
     # Creating a TCP socket for sending messages
     sendingSocket = socket(AF_INET, SOCK_STREAM)
     sendingSocket.connect((HOST, clientPort))
     
+    
+    
+    
+    # Perform diffie-hellman key exchange before sending messages
+    diffie_hellman(sendingSocket, connectionSocket)
+    
+    
+    
+    
     # Create two threads, one for receiving messages and one for sending messages
     #   - two seperate threads so that we can send and receive messages at the same time
-    rcvThread = threading.Thread(target = recv_from_client, args = (connectionSocket,))
-    sndThread = threading.Thread(target = send_to_client, args = (sendingSocket,))
+    #rcvThread = threading.Thread(target = recv_from_client, args = (connectionSocket,))
+    #sndThread = threading.Thread(target = send_to_client, args = (sendingSocket,))
     
     # Start the sending and receiving threads
     # Setting a daemons so that threads are killed when main program exits
-    sndThread.daemon = True
-    rcvThread.daemon = True
-    rcvThread.start()
-    sndThread.start()
+    #sndThread.daemon = True
+    #rcvThread.daemon = True
+    #rcvThread.start()
+    #sndThread.start()
     
     # Close the program when a quit message is sent or recieved
-    while(closeConnection == False):
-        if(closeConnection == True):
-            print('Program finished')
-            sys.exit()
+    #while(closeConnection == False):
+    #    if(closeConnection == True):
+    #        print('Program finished')
+    #        sys.exit()
 
 
 
